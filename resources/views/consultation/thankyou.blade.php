439dfb0c8966447fdd78d9fa7adf953ee7a7d282
daviddikenshteinMAC <dikenshtein@bmpgmbh.de>
2025-10-11 18:46:01 +0200

    thankusite

--- resources/views/consultation/thankyou.blade.php
+++ resources/views/consultation/thankyou.blade.php
@@ -4,15 +4,19 @@
 @section('meta_description', "Дякуємо за заявку")
 
 @section('content')
-    <section class="hero">
-        <h1 class="h1">Дякуємо за заявку!</h1>
-        <p class="sub">Ми зв'яжемося з вами найближчим часом.</p>
-        <p style="margin-top:16px">Вас буде автоматично перенаправлено на головну через 20 секунд.</p>
+    <section class="thankyou">
+        <div class="thankyou-card glass">
+            <div class="thankyou-icon">&#10003;</div>
+            <h1>Дякуємо за заявку!</h1>
+            <p class="thankyou-sub">Ми вже отримали вашу заявку і зв'яжемося з вами найближчим часом.</p>
+            <a href="{{ route('home') }}" class="btn thankyou-btn">Повернутися на головну</a>
+            <p class="thankyou-meta">Вас буде автоматично перенаправлено на головну через 10 секунд.</p>
+        </div>
     </section>
 
     <script>
         setTimeout(function () {
             window.location.href = "{{ route('home') }}";
-        }, 20000);
+        }, 10000);
     </script>
 @endsection
