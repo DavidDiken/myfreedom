163c4a0a87940b9d9de09600bb474130159d4cf7
daviddikenshteinMAC <dikenshtein@bmpgmbh.de>
2025-10-11 18:39:57 +0200

    Правки и фавиконка

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,11 @@
     <link rel="canonical" href="{{ url()->current() }}">
     <title>@yield('title', 'MYFREEDOM')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 </head>
 <body>
 <nav class="nav">
